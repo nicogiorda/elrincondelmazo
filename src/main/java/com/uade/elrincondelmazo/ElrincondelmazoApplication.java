847623d43a15e627ce8543c3e1953cf0a5d9006a
nicogiorda <nicolasgiordano1505@gmail.com>
2026-08-18 15:45:54 -0300

    Creacion del repositorio

---
+++ src/main/java/com/uade/elrincondelmazo/ElrincondelmazoApplication.java
@@ -0,0 +1,13 @@
+package com.uade.elrincondelmazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElrincondelmazoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElrincondelmazoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/elrincondelmazo/ElrincondelmazoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.elrincondelmazo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElrincondelmazoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
