ecb40e18c1e34806157c7905c284410d8f37a71e
nicogiorda <nicolasgiordano1505@gmail.com>
2026-08-20 15:50:18 -0300

    Agregue @Repository a CollectionRepository

--- src/main/java/com/uade/elrincondelmazo/repository/CollectionRepository.java
+++ src/main/java/com/uade/elrincondelmazo/repository/CollectionRepository.java
@@ -1,9 +1,11 @@
 package com.uade.elrincondelmazo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uade.elrincondelmazo.entity.Collection;
 
+@Repository
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
     
 }
