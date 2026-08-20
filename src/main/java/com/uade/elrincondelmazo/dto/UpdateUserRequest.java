71b217200d65b57417de28113692b440d644bb08
ThomBer17 <thomybergamo@gmail.com>
2026-08-20 15:08:38 -0300

    updates de los usuairios, repository, service, controller, nueva excepción y contenido en el dto

---
+++ src/main/java/com/uade/elrincondelmazo/dto/UpdateUserRequest.java
@@ -0,0 +1,12 @@
+package com.uade.elrincondelmazo.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateUserRequest {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+
+}
