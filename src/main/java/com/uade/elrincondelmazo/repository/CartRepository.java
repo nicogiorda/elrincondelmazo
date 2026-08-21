76b7b16cd2faf422aa5f03ef714fbff9aeacd69e
juannborras <juborras@uade.edu.ar>
2026-08-21 09:14:28 -0300

    Implementacion de operaciones de CartRepository e ItemCartRepository.

--- src/main/java/com/uade/elrincondelmazo/repository/CartRepository.java
+++ src/main/java/com/uade/elrincondelmazo/repository/CartRepository.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import com.uade.elrincondelmazo.entity.Cart;
 
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
-    
+
+    ///El CRUD basico se hereda de JpaRepository, pero podemos agregar metodos personalizados si es necesario
+
+    Optional<Cart> findByUserId(Long userId);
+
 }
