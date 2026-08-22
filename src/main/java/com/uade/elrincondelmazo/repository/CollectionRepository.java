afe1ad37297f9186637419ebb9bd6f44069482e9
vdavidson1234 <vdavidson@uade.edu.ar>
2026-08-22 14:27:46 -0300

    update

--- src/main/java/com/uade/elrincondelmazo/repository/CollectionRepository.java
+++ src/main/java/com/uade/elrincondelmazo/repository/CollectionRepository.java
@@ -1,5 +1,7 @@
 package com.uade.elrincondelmazo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.uade.elrincondelmazo.entity.Collection;
 
 @Repository
 public interface CollectionRepository extends JpaRepository<Collection, Long> {
+    Optional<Collection> findByName(String name);
     
+    boolean existsByName(String name);
 }
