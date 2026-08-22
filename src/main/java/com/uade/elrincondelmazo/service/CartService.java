4e100010a464b97fee2543727e9772e9c9d36a19
juannborras <juborras@uade.edu.ar>
2026-08-22 01:27:54 -0300

    Implementacion de metodo para agregar un item al carrito

--- src/main/java/com/uade/elrincondelmazo/service/CartService.java
+++ src/main/java/com/uade/elrincondelmazo/service/CartService.java
@@ -1,9 +1,12 @@
 package com.uade.elrincondelmazo.service;
 
+import com.uade.elrincondelmazo.entity.dto.AddCartItemRequest;
 import com.uade.elrincondelmazo.entity.dto.CartResponse;
 
 public interface CartService {
 
     CartResponse getOrCreateCart(Long userId);
+
+    CartResponse addItem(Long userId, AddCartItemRequest request);
 
 }
