baea42c212a8a176f9c1cddab98acd3cb0b34052
nicogiorda <ngiordano@uade.edu.ar>
2026-09-02 10:04:41 -0300

    Agregar verificación de estado habilitado del usuario en JwtAuthenticationFilter

--- src/main/java/com/uade/elrincondelmazo/controllers/config/JwtAuthenticationFilter.java
+++ src/main/java/com/uade/elrincondelmazo/controllers/config/JwtAuthenticationFilter.java
@@ -39,7 +39,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         userEmail = jwtService.extractEmail(jwt);
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(jwt, userDetails)) {
+            if (jwtService.isTokenValid(jwt, userDetails) && userDetails.isEnabled()) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
                         null,
