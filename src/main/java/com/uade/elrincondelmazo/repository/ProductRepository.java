8f7830dda3302c02f38e53dbd6324af48dcd0371
nicogiorda <ngiordano@uade.edu.ar>
2026-09-04 12:03:36 -0300

    Agregue filtrado de estado de producto y maneje productos eliminados en ProductService

--- src/main/java/com/uade/elrincondelmazo/repository/ProductRepository.java
+++ src/main/java/com/uade/elrincondelmazo/repository/ProductRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.uade.elrincondelmazo.enums.ProductStatus;
 
 import com.uade.elrincondelmazo.entity.Product;
 import com.uade.elrincondelmazo.enums.ProductType;
@@ -16,13 +17,14 @@ import com.uade.elrincondelmazo.enums.ProductType;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     @Query("""
-                SELECT p FROM Product p
-                WHERE (:search IS NULL
-                    OR LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%')))
-                AND (:type IS NULL OR p.type = :type)
-                AND (:collectionId IS NULL OR p.collection.id = :collectionId)
-                AND (:minPrice IS NULL OR p.price >= :minPrice)
-                AND (:maxPrice IS NULL OR p.price <= :maxPrice)
+            SELECT p FROM Product p
+            WHERE p.status <> :excludedStatus
+            AND (:search IS NULL
+                OR LOWER(p.name) LIKE LOWER(CONCAT('%', :search, '%')))
+            AND (:type IS NULL OR p.type = :type)
+            AND (:collectionId IS NULL OR p.collection.id = :collectionId)
+            AND (:minPrice IS NULL OR p.price >= :minPrice)
+            AND (:maxPrice IS NULL OR p.price <= :maxPrice)
             """)
     Page<Product> findProducts(
             @Param("search") String search,
@@ -30,9 +32,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             @Param("collectionId") Long collectionId,
             @Param("minPrice") BigDecimal minPrice,
             @Param("maxPrice") BigDecimal maxPrice,
+            @Param("excludedStatus") ProductStatus excludedStatus,
             Pageable pageable);
 
     Page<Product> findBySellerId(
-        Long sellerId, 
-        Pageable pageable);
+            Long sellerId,
+            Pageable pageable);
 }
