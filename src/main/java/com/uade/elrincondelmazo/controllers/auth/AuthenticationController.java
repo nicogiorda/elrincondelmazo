533313a21536360e09b38d3709bf7d9b98eed1bf
nicogiorda <nicolasgiordano1505@gmail.com>
2026-09-05 18:10:12 -0300

    Agregar anotaciones de validación a los modelos de solicitud de autenticación y registro

--- src/main/java/com/uade/elrincondelmazo/controllers/auth/AuthenticationController.java
+++ src/main/java/com/uade/elrincondelmazo/controllers/auth/AuthenticationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.uade.elrincondelmazo.service.AuthenticationService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,13 +20,17 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(service.register(request));
+            @Valid @RequestBody RegisterRequest request) {
+
+        return ResponseEntity.ok(
+                service.register(request));
     }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
-            @RequestBody AuthenticationRequest request) {
-        return ResponseEntity.ok(service.authenticate(request));
+            @Valid @RequestBody AuthenticationRequest request) {
+
+        return ResponseEntity.ok(
+                service.authenticate(request));
     }
 }
